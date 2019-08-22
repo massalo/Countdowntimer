b5b3e7055be0645d07d3bcd17471131d557ae56d
massalo <massalo@mauanda.com>
2019-08-22 17:08:36 +0100

    Countdown and stopwatch Timer using Flutter

---
+++ lib/main.dart
@@ -0,0 +1,298 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:numberpicker/numberpicker.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Time",
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: Homepage(),
+    );
+  }
+}
+
+class Homepage extends StatefulWidget {
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> with TickerProviderStateMixin {
+  TabController tb;
+  int hour = 0;
+  int min = 0;
+  int sec = 0;
+  bool started = true;
+  bool stopped = true;
+  int timeForTimer = 0;
+  String timetodisplay = "";
+  bool checktimer = true;
+
+  void initState() {
+    tb = TabController(
+      length: 2,
+      vsync: this,
+    );
+    super.initState();
+  }
+
+  void start() {
+    setState(() {
+      started = false;
+      stopped = false;
+    });
+    timeForTimer = ((hour * 60 * 60) + (min * 60) + sec);
+    Timer.periodic(
+        Duration(
+          seconds: 1,
+        ), (Timer t) {
+      setState(() {
+        if (timeForTimer < 1 || checktimer == false) {
+          t.cancel();
+          if (timeForTimer == 0) {
+            //INSERT MUSIC OR NEW SCREEN OR TOAST HERE
+            Fluttertoast.showToast(
+                msg: "Tempo Terminado!",
+                toastLength: Toast.LENGTH_SHORT,
+                gravity: ToastGravity.BOTTOM,
+                timeInSecForIos: 1,
+                backgroundColor: Colors.red,
+                textColor: Colors.white,
+                fontSize: 16.0);
+          }
+          Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Homepage(),
+              ));
+        }
+        //converts time remaining in legible form hh:mm:ss
+        else if (timeForTimer < 60) {
+          timetodisplay = timeForTimer.toString();
+          timeForTimer = timeForTimer - 1;
+        } else if (timeForTimer < 3600) {
+          int m = timeForTimer ~/ 60;
+          int s = timeForTimer - (60 * m);
+          timetodisplay = m.toString() + ":" + s.toString();
+          timeForTimer = timeForTimer - 1;
+        } else {
+          int h = timeForTimer ~/ 3600;
+          int t = timeForTimer - (3600 * h);
+          int m = t ~/ 60;
+          int s = t - (60 * m);
+          timetodisplay =
+              h.toString() + ":" + m.toString() + ":" + s.toString();
+          timeForTimer = timeForTimer - 1;
+        }
+      });
+    });
+  }
+
+  void stop() {
+    setState(() {
+      started = true;
+      stopped = true;
+      checktimer = false;
+    });
+  }
+
+  Widget timer() {
+    return Container(
+      color: Colors.grey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            flex: 6,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(
+                        bottom: 10.0,
+                      ),
+                      child: Text(
+                        "HH",
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                    NumberPicker.integer(
+                      initialValue: hour,
+                      minValue: 0,
+                      maxValue: 23,
+                      listViewWidth: 60.0,
+                      onChanged: (val) {
+                        setState(() {
+                          hour = val;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(
+                        bottom: 10.0,
+                      ),
+                      child: Text(
+                        "MM",
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                    NumberPicker.integer(
+                      initialValue: min,
+                      minValue: 0,
+                      maxValue: 59,
+                      listViewWidth: 60.0,
+                      onChanged: (val) {
+                        setState(() {
+                          min = val;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(
+                        bottom: 10.0,
+                      ),
+                      child: Text(
+                        "SS",
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                    NumberPicker.integer(
+                      initialValue: sec,
+                      minValue: 0,
+                      maxValue: 59,
+                      listViewWidth: 60.0,
+                      onChanged: (val) {
+                        setState(() {
+                          sec = val;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Text(
+              timetodisplay,
+              style: TextStyle(
+                fontSize: 35.0,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                RaisedButton(
+                  onPressed: started ? start : null,
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 40.0,
+                    vertical: 10.0,
+                  ),
+                  color: Colors.green,
+                  child: Text(
+                    "Start",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white60,
+                    ),
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                ),
+                RaisedButton(
+                  onPressed: stopped ? null : stop,
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 40.0,
+                    vertical: 10.0,
+                  ),
+                  color: Colors.red,
+                  child: Text(
+                    "Stop",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white60,
+                    ),
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "PURO RELÓGIO",
+        ),
+        centerTitle: true,
+        bottom: TabBar(
+          tabs: <Widget>[
+            Text(
+              "Temporizador",
+            ),
+            Text("Contador"),
+          ],
+          labelPadding: EdgeInsets.only(
+            bottom: 10.0,
+          ),
+          labelStyle: TextStyle(fontSize: 18.0),
+          unselectedLabelColor: Colors.white60,
+          controller: tb,
+        ),
+      ),
+      body: TabBarView(
+        children: <Widget>[
+          timer(),
+          Text("Contador"),
+        ],
+        controller: tb,
+      ),
+    );
+  }
+}
